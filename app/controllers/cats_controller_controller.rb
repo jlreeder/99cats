4433323fa6e92742f0f56c818361b70bd0743ab1
Jason Reeder <info@jasonreeder.com>
2017-02-14 11:21:08 -0800

    Add methods to CatsController

--- app/controllers/cats_controller_controller.rb
+++ app/controllers/cats_controller_controller.rb
@@ -1,16 +1,38 @@
 class CatsControllerController < ApplicationController
   def index
-    @cats = Cat.all
-    render :index
+    render :text "Hello World I'm a cat"
+    # @cats = Cat.all
+    # render :index
   end
 
   def show
     @cat = Cat.find(params[:id])
     if @cat
-      render cat_url(@cat)
+      # NOTE: When do we use `cat_url(@cat)`?
+      render :show
     else
       flash.now[:errors] = @cat.errors.full_messages
       redirect_to :index
     end
+  end
+
+  def new
+    render :new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      render :show
+    else
+      flash.now[:errors] = @cat.errors.full_messages
+      redirect_to :new
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :cats
+  # resources :cats
+  resources :cats, only: [:new, :create, :index, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
